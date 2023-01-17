4265189c3c901b2eaf45c6e63fcb860f34f41dc0
GEDAHISA <106118710+GEDAHISA@users.noreply.github.com>
2023-01-16 21:29:23 -0300

    treino

--- AULA 04/Script.js
+++ AULA 04/Script.js
@@ -65,7 +65,51 @@
 
 // }
 
-const numeros = [1,2,10,3,4,5];
-numeros.sort((a,b) => a-b)
+// const numeros = [1,2,10,3,4,5];
+// numeros.sort((a,b) => a-b)
 
 // como sortear datas
+
+// const array = [0, 1, 2+2 ==4,"ameixa",5,6,7,8,9,"bombom"]
+// console.log(array[array.length -1]);
+
+// const array= [0, 1, 2+2 ==4,"ameixa",5,6,7,8,9,"bombom"]
+// for (let i = 0; i< array.length; i++) {
+//     const element = array[array];
+//     console.log(array[i]);
+// }
+
+// const nomes = ['ana',"maria","jurema","alba"];
+// nomes.forEach((nome)=> console.log(nome.toUpperCase())) ;
+
+const abacaxi = [0, 1, 2+2 ==4,"ameixa",5,6,7,8,9,"bombom"]
+const abacaxiMaduro= abacaxi.map(_ =>2)
+// console.log(abacaxiMaduro)
+
+const numeros = abacaxiMaduro.map((value)=>value + Math.floor(Math.random()*10));
+// console.log(numeros);
+
+// for (let index = 0; index < numeros.length; index++) {
+//     const element = numeros[index];
+//     if (element %2 !== 0) {
+//        console.log(element) 
+//     }
+    
+// }
+
+// numeros.forEach(element => { 
+//     if (element > 4) {
+//         console.log(element)  
+//    
+// })
+
+const pares = numeros.filter(numero => numero %2 ===0)
+console.log(pares)
+
+const cores = ["verde","amarelo","laranja","vermelho","azul" ] 
+cores.forEach(cor => console.log(cor.toUpperCase()))
+
+cores.splice(cores.indexOf("amarelo"),1);
+console.log(cores);
+
+cores.forEach(cor =>console.log(cor[0].toUpperCase()+cor.substring(1)))
